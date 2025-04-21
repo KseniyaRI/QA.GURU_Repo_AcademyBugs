87fc1190407faa29930e169bf74176fe842e8ec8
Ksusha_Ryabukhina <r1995k@mail.ru>
2025-04-21 16:01:35 +0300

    update

--- sources/pages/accountPage.js
+++ sources/pages/accountPage.js
@@ -3,18 +3,19 @@ export class AccountPage{
         // this.billingInformationLink = page.locator('.ec_account_dashboard_link.ec_cart_input_row')
         // this.billingInformationLink = page.locator('#ec_account_dashboard_link') 
         this.billingInformationLink = page.getByRole('link', { name: 'billing information' })
-        this.billingAddressLoader = page.locator('[id^="ec_cart_billing_info_update_loader"]')
+        // this.billingAddressLoader = page.locator('[id^="ec_cart_billing_info_update_loader"]')
+        this.billingAddressLoader = page.locator('.ec_cart_billing_info_update_loader')
     }
 
     async gotoBillingAddress(){
-        await this.billingAddressLoader.waitFor({ state: 'attached' });
-        await this.billingAddressLoader.waitFor({ state: 'visible' });
+        //await this.billingAddressLoader.waitFor({ state: 'attached' });
+        //await this.billingAddressLoader.waitFor({ state: 'visible' });
         await this.billingAddressLoader.click({ force: true });
     }
 
     async gotoBillingInfo(){
-        await this.billingInformationLink.waitFor({ state: 'attached' });
-        await this.billingInformationLink.waitFor({ state: 'visible' });
+        //await this.billingInformationLink.waitFor({ state: 'attached' });
+        //await this.billingInformationLink.waitFor({ state: 'visible' });
         await this.billingInformationLink.click({ force: true });
     }
 }
--- sources/pages/bugPopupsPage.js
+++ sources/pages/bugPopupsPage.js
@@ -1,9 +1,9 @@
 export class BugPopupsPage {
     constructor(page) {
         this.bugPopupCongrats = page.locator('.academy-popup-bug-title')
-        this.bugPopupQuestion = page.locator('.bug-popup')
+        this.bugPopupQuestion = page.locator('#bug-popup')
         this.bugLayerInfo = page.locator('.academy-bug-info-overlay')
-        this.bugLayer = page.locator('.academy-bug-overlay')
+        this.bugLayer = page.locator('#academy-bug-overlay')
         // this.bugLayerHeading = page.locator('h5');
         this.crashLayer = page.locator('.academy-crash-overlay-bug')
     }
